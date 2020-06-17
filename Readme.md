0d2b415d6ab735e0863667f50ef5bab043ba320c
wohshon <wohshon@gmail.com>
2020-06-17 23:04:13 +0800

    v2

---
+++ Readme.md
@@ -0,0 +1,2 @@
+curl -X POST -H 'Content-type: application/json' http://order-mesh.apps.cluster-sgp-75d2.sgp-75d2.example.opentlc.com/submit -d '{"id": "123","status":"new", "product": "apple", "qty":500}'
+
--- order/routes/index.js
+++ order/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/order', function(req, res, next) {
+router.get('/hello', function(req, res, next) {
   res.send("hello-v2");
 });
 module.exports = router;
