601839d03a52e5231e7e2f7eea2aac4c388e690a
wohshon <wohshon@gmail.com>
2020-06-17 23:11:39 +0800

    v1

--- order/routes/index.js
+++ order/routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/order', function(req, res, next) {
-  res.send("hello");
+router.get('/hello', function(req, res, next) {
+  res.send("hello-v1");
 });
 module.exports = router;
 
